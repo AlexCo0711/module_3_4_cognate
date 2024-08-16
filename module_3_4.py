dc43db501750b303a72f5d62aae3b7400ebfbc5b
AlexCo0711 <alexco@list.ru>
2024-08-16 14:34:25 +0300

    Add files via upload
    Самостоятельная работа по уроку "Произвольное число параметров".

---
+++ module_3_4.py
@@ -0,0 +1,23 @@
+# Самостоятельная работа по уроку "Произвольное число параметров".
+
+# Объявление функции single_root_words
+def single_root_words(root_word, *other_words):
+# создание пустого списка
+    same_words = []
+# приведение к нижнему регистру параметра root_word
+    root_word = root_word.lower()
+# цикл поиска *other_words в root_word или root_word в *other_words
+    for i in (other_words):
+# Условие для поиска
+         if i.count(root_word) == True or root_word.count(i.lower()) == True:
+# Добавление найденных элементов в конец списока same_word
+            same_words.append(i)
+# вывод результата работы функции
+    return same_words
+# Исходный код
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+# Вывод на консоль результата работы фунции single_root_words для каждого вхождения
+print(result1)
+print(result2)
